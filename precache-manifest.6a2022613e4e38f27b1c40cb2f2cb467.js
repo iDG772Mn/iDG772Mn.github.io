beb79ab858f9ed2fd4aa80a3bc80c6231435ac53
Valerie Cheng <45530922+iDG772Mn@users.noreply.github.com>
2019-01-03 18:56:37 +0800

    Deploy Site

---
+++ precache-manifest.6a2022613e4e38f27b1c40cb2f2cb467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4311eb8fc2b5eb58baa0",
+    "url": "/static/css/main.5a5e8576.chunk.css"
+  },
+  {
+    "revision": "4311eb8fc2b5eb58baa0",
+    "url": "/static/js/main.4311eb8f.chunk.js"
+  },
+  {
+    "revision": "5cd5b0727272f929f876",
+    "url": "/static/css/1.fe34edfd.chunk.css"
+  },
+  {
+    "revision": "5cd5b0727272f929f876",
+    "url": "/static/js/1.5cd5b072.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8186a768e4134892b933e6c9010fddf2",
+    "url": "/index.html"
+  }
+];
